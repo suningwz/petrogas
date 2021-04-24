8a51ad51bec8fe43d75798cebcc62f6dbe4c62c2
soilkane <aly.kane@staroilgroup.com>
2021-04-24 17:53:52 +0000

    SMP Inventory Update

--- smp_inventory/models/product.py
+++ smp_inventory/models/product.py
@@ -3,6 +3,8 @@ from datetime import date,timedelta
 from odoo import models, fields, api, exceptions, _, tools
 from  odoo.exceptions import ValidationError
 from odoo.addons import decimal_precision as dp
+from odoo.tools.float_utils import float_compare, float_round, float_is_zero
+
 
 
 class ProductTemplate(models.Model):
@@ -74,3 +76,39 @@ class ProductTemplate(models.Model):
             self.inter_uom_factor = None
         else:
             self.is_uom_inter_category = True
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def _select_seller(self, partner_id=False, quantity=0.0, date=None, uom_id=False, params=False):
+        self.ensure_one()
+        if date is None:
+            date = fields.Date.context_today(self)
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+
+        res = self.env['product.supplierinfo']
+        sellers = self._prepare_sellers(params)
+        if self.env.context.get('force_company'):
+            sellers = sellers.filtered(lambda s: not s.company_id or s.company_id.id == self.env.context['force_company'])
+        for seller in sellers:
+            # Set quantity in UoM of seller
+            quantity_uom_seller = quantity
+            if quantity_uom_seller and uom_id and uom_id != seller.product_uom:
+                quantity_uom_seller = uom_id.with_context(inter_uom_factor=self.inter_uom_factor)._compute_quantity(quantity_uom_seller, seller.product_uom)
+
+            if seller.date_start and seller.date_start > date:
+                continue
+            if seller.date_end and seller.date_end < date:
+                continue
+            if partner_id and seller.name not in [partner_id, partner_id.parent_id]:
+                continue
+            if float_compare(quantity_uom_seller, seller.min_qty, precision_digits=precision) == -1:
+                continue
+            if seller.product_id and seller.product_id != self:
+                continue
+
+            res |= seller
+            break
+        return res
